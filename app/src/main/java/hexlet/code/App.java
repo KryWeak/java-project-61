b1fa95f456063afe40a812a29264a64924e6d2ff
KryWeak <kmoretti999@gmail.com>
2025-05-13 13:15:20 +0300

    Added new game 'Even' and new class Engine

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the game number and press Enter."
+                + "\n 1 - Greet"
+                + "\n 2 - Even"
+                + "\n 0 - Exit"
+                + "\n Your choice: ");
+        int numGame = scanner.nextInt();
+
+        switch (numGame) {
+            case 1 -> Cli.greeting();
+            case 2 -> Even.gameEven();
+
+
+            default -> { }
+        }
+
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,43 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void gameEven() {
+        Engine.greetingUser();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        var count = 0;
+        var finalCount = 3;
+
+
+        for (int i = 0; i < finalCount; i++) {
+            int rundomNum = Engine.randomNumForEven();
+            System.out.println("\nQuestion: " + rundomNum);
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.nextLine().trim().toLowerCase();
+            String even = "";
+
+            if (rundomNum % 2 == 0) {
+                even = "yes";
+            } else {
+                even = "no";
+            }
+
+            if (userAnswer.equals(even)) {
+                Engine.trueAns();
+                count++;
+            } else {
+                Engine.falseWord(userAnswer, even);
+                break;
+            }
+
+        }
+        if (count == finalCount) {
+            Engine.userWin();
+        }
+
+    }
+}
