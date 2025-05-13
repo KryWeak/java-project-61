e8b174ee9a3ddbd72df202f84447adb4fe2e7900
KryWeak <kmoretti999@gmail.com>
2025-05-13 19:12:43 +0300

    Addde new game 'GCD' and tweaked the app class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,6 +10,7 @@ class App {
                 + "\n 1 - Greet"
                 + "\n 2 - Even"
                 + "\n 3 - Calc"
+                + "\n 4 - GCD"
                 + "\n 0 - Exit"
                 + "\n Your choice: ");
         int numGame = scanner.nextInt();
@@ -18,6 +19,7 @@ class App {
             case 1 -> Cli.greeting();
             case 2 -> Even.gameEven();
             case 3 -> Calc.gameCalc();
+            case 4 -> GCD.gameGCD();
 
             default -> { }
         }
---
+++ app/src/main/java/hexlet/code/GCD.java
@@ -0,0 +1,46 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class GCD {
+
+    public static void gameGCD() {
+        Engine.greetingUser();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        int count = 0;
+        int finalCount = 3;
+
+        for (int i = 0; i < finalCount; i++) {
+
+            int firstNum = Engine.randomNum();
+            int secondNum = Engine.randomNum();
+            System.out.println("Question: " + firstNum + " " + secondNum);
+            System.out.print("Your answer: ");
+            int userAnswer = scanner.nextInt();
+
+            while (secondNum != 0) {
+                int temp = secondNum;
+                secondNum = firstNum % secondNum;
+                firstNum = temp;
+            }
+
+            int correctAnswer = firstNum;
+
+            if (correctAnswer == userAnswer) {
+                count++;
+                Engine.trueAns();
+            } else {
+                Engine.falseNums(userAnswer, correctAnswer);
+                break;
+            }
+
+        }
+
+        if (count == finalCount) {
+            Engine.userWin();
+        }
+
+    }
+}
